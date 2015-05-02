7e5f0c9f43e94b66290f6a20fc63a829c42ec5c7
Osias Maia <osiasjota@gmail.com>
2015-05-02 14:35:44 -0300

    get_balances:    Parameter address is already an array.    Fixed indentation

--- ParseCloudCode/cloud/main.js
+++ ParseCloudCode/cloud/main.js
@@ -1,42 +1,42 @@
 Parse.Cloud.define("hello", function (request, response) {
-                   response.success("Hello world!");
-                   });
+    response.success("Hello world!");
+});
 
 
 Parse.Cloud.define("get_balances", function (request, response) {
-                   
-                   Parse.Cloud.httpRequest({
-                                           method: 'POST',
-                                           url: 'http://counterparty:1234@xcp-dev.vennd.io:4000/api/',
-                                           body:
-                                           {
-                                           "jsonrpc": "2.0",
-                                           "id": 0,
-                                           "method": "get_balances",
-                                           "params": {
-                                           "filters": {
-                                           "field": "address",
-                                           "op": "IN",
-                                           "value": [ request.params.address ]
-                                           }
-                                           }
-                                           },
-                                           
-                                           headers: {
-                                           'Content-Type': 'application/json;charset=utf-8',
-                                           'Accept': 'application/json, text/javascript'
-                                           },
-                                           success: function (httpResponse) {
-                                           response.success(httpResponse.data);
-                                           },
-                                           error: function (httpResponse) {
-                                           response.success(
-                                                            "Request failed with response code " +
-                                                            httpResponse.status +
-                                                            "\n" +
-                                                            httpResponse.text
-                                                            );
-                                           }
-                                           });
-                   
-                   });
+
+    Parse.Cloud.httpRequest({
+        method: 'POST',
+        url: 'http://counterparty:1234@xcp-dev.vennd.io:4000/api/',
+        body:
+          {
+              "jsonrpc": "2.0",
+              "id": 0,
+              "method": "get_balances",
+              "params": {
+                  "filters": {
+                      "field": "address",
+                      "op": "IN",
+                      "value": request.params.address
+                  }
+              }
+          },
+
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Accept': 'application/json, text/javascript'
+        },
+        success: function (httpResponse) {
+            response.success(httpResponse.data);
+        },
+        error: function (httpResponse) {
+            response.success(
+                "Request failed with response code " +
+                httpResponse.status +
+                "\n" +
+                httpResponse.text
+                );
+        }
+    });
+
+});
