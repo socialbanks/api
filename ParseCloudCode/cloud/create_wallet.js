baa620a7a337ded9398fb10bf15b178574fb74c8
Fabricio Matos <fabriciovargasmatos@gmail.com>
2015-05-03 03:20:26 -0300

    bitcore.js lib added

---
+++ ParseCloudCode/cloud/create_wallet.js
@@ -0,0 +1,6 @@
+//var bitcore = require('cloud/bitcore/index.js');
+
+exports.func = function (request, response) {
+  response.success("create wallet!");
+}
+
--- ParseCloudCode/cloud/main.js
+++ ParseCloudCode/cloud/main.js
@@ -6,3 +6,7 @@ Parse.Cloud.define("get_balances", get_balances.func);
 
 var create_issuance = require('cloud/create_issuance.js');
 Parse.Cloud.define("create_issuance", create_issuance.func);
+
+var create_wallet = require('cloud/create_wallet.js');
+Parse.Cloud.define("create_wallet", create_wallet.func);
+
